2997f2678e37f6651490b8d084be41213d4315f8
Jacob Manuel <Jacob.Manuel@canada.ca>
2020-12-12 22:54:40 -0400

    .

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ window.onload = () => {
 
   if('serviceWorker' in navigator) {
     navigator.serviceWorker
-      .register('./service-worker.js')
+      .register(
+        './service-worker.js',
+        { scope: "/esf-pwa" }
+      )
       .then(() => console.log('Service Worker Registered'))
       .catch(e => console.log(e))
   } else {
