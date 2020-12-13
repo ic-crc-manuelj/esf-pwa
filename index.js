f82935985b6398a0683387b2f5cb78cefe30c811
Jacob Manuel <Jacob.Manuel@canada.ca>
2020-12-12 22:47:09 -0400

    fix: again

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ window.onload = () => {
 
   if('serviceWorker' in navigator) {
     navigator.serviceWorker
-             .register('/esf-pwa/service-worker.js')
+             .register(
+               '/esf-pwa/service-worker.js',
+               { scope: '/esf-pwa/'}
+              )
              .then(() => console.log('Service Worker Registered'))
              .catch(e => console.log(e))
   } else {
